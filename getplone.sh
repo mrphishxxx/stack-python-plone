2b53d219690250bfe518cdab1ac6829f0c34b799
David Bain <pigeonflight@gmail.com>
2012-11-11 11:20:33 -0500

    updated getplone to pull eggs from dropbox

--- getplone.sh
+++ getplone.sh
@@ -2,6 +2,7 @@
 virtualenv -p python2.7 .
 bin/pip install zc.buildout
 bin/buildout init
-wget https://dl.dropbox.com/u/1004432/plonewsgi.cfg -O buildout.cfg
+wget http://dl.dropbox.com/u/1004432/plonewsgi.cfg -O buildout.cfg
+wget http://dl.dropbox.com/u/1004432/dotcloudplone/eggs.tgz -O eggs.tgz
 tar xfz eggs.tgz
 bin/buildout
