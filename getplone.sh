2a5c9fc255148f7413837e8eb02186db71fa29e7
David Bain <pigeonflight@gmail.com>
2012-11-12 20:03:44 -0500

    updated configuration for deployment

--- getplone.sh
+++ getplone.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
-virtualenv -p python2.7 .
-bin/pip install zc.buildout
-bin/buildout init
+# we have buildout based on settings in requirements.txt
+buildout init
 wget http://dl.dropbox.com/u/1004432/plonewsgi.cfg -O buildout.cfg
+
+# get eggs previously compiled for python2.7 on a dotcloud machine
 wget http://dl.dropbox.com/u/1004432/dotcloudplone/eggs.tgz -O eggs.tgz
 tar xfz eggs.tgz
-bin/buildout
+buildout
