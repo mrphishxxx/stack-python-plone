4c4e5c4b4ccfe291a55d593e1b7ed4ff0cb20593
David Bain <pigeonflight@gmail.com>
2012-11-13 07:37:11 -0500

    added new configuration files for nginx and supervisord.conf and we've "abused" the nginx setup to inject a location override via plone-uwsgi.conf

--- getplone.sh
+++ getplone.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 # we have buildout based on settings in requirements.txt
 buildout init
-wget http://dl.dropbox.com/u/1004432/plonewsgi.cfg -O buildout.cfg
+wget http://dl.dropbox.com/u/1004432/plone.cfg -O buildout.cfg
 
 # get eggs previously compiled for python2.7 on a dotcloud machine
-wget http://dl.dropbox.com/u/1004432/dotcloudplone/eggs.tgz -O eggs.tgz
+wget http://dl.dropbox.com/u/1004432/eggs.tgz -O eggs.tgz
 tar xfz eggs.tgz
 buildout
