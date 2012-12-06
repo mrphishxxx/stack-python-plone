6328e5d5a21ba40ece71dcef59ca35c126a9d907
David Bain <david@alteroo.com>
2012-12-06 05:11:10 -0500

    updated getplone.sh

--- getplone.sh
+++ getplone.sh
@@ -2,13 +2,13 @@
 # we have buildout based on settings in requirements.txt
 buildout init
 wget http://dl.dropbox.com/u/1004432/plone.cfg -O buildout.cfg
+mkdir -p /home/dotcloud/data
 
 # get eggs previously compiled for python2.7 on a dotcloud machine
 if [ ! -f /home/dotcloud/data/eggs.tgz ];                                                                                                       
 then                                                                                                                        
     echo "Downloading egg bundle" 
-    wget http://dl.dropbox.com/u/1004432/eggs.tgz -O /home/dotcloud/data/eggs.tgz
-    mkdir -p /home/dotcloud/data
+    wget http://dl.dropbox.com/u/1004432/eggs.tgz -O /home/dotcloud/data/eggs.tgz    
     cd /home/dotcloud/data
     tar xfz eggs.tgz
 fi
